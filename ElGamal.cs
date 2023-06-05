edc6ded0f3b1fb243583c3b2c7b0b96bc91f8d45
Maryna Parkhomenko <marina.parkhomenko.02@gmail.com>
2023-06-05 16:40:54 +0300

    first commit

---
+++ ElGamal.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace lab3
+{
+    public class ElGamal
+    {
+        private int KeySize;
+
+        public ElGamal(int keySize)
+        {
+            KeySize = keySize;
+        }
+
+
+        public uint GenerateRandomPrime(int bits)
+        {
+            var rng = new RNGCryptoServiceProvider();
+            var bytes = new byte[bits / 4];
+            int prime;
+            do
+            {
+                rng.GetBytes(bytes);
+                
+                prime = BitConverter.ToInt32(bytes, 0);
+            } while (!IsPrime(prime));
+
+            return (uint)prime;
+        }
+
+        public bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public uint GenerateRandomNumber(uint min, uint max)
+        {
+            var rng = new RNGCryptoServiceProvider();
+            var bytes = new byte[4];
+            rng.GetBytes(bytes);
+            int randomNumber = BitConverter.ToInt32(bytes, 0);
+
+            return (uint)((randomNumber % (max - min + 1)) + min);
+        }
+
+        public uint ModuloPower(uint number, uint power, uint modulus)
+        {
+            if (power == 0)
+                return 1;
+
+            uint result = 1;
+
+            while (power > 0)
+            {
+                if (power % 2 == 1)
+                    result = (result * number) % modulus;
+
+                number = (number * number) % modulus;
+                power >>= 1;
+            }
+
+            return (uint)result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace lab3
+{
+    public class Program
+    {
+        static void Main()
+        {
+            {
+                int keySize = 16; // 16-bit key size
+
+                ElGamal elGamal = new ElGamal(keySize);
+
+                uint p = elGamal.GenerateRandomPrime(keySize);
+                uint g = elGamal.GenerateRandomNumber(2, p - 2);
+                uint a = elGamal.GenerateRandomNumber(1, p - 1);
+                uint A = elGamal.ModuloPower(g, a, p);
+                // Generate random prime number (p)
+                //var p = GenerateRandomPrime(keySize);
+
+                // Generate random number (g) between 2 and p-2
+                //var g = GenerateRandomNumber(2, p - 2);
+
+                // Generate private key (a) between 1 and p-1
+                //var a = GenerateRandomNumber(1, p - 1);
+
+                // Calculate public key (A)
+                //var A = ModuloPower(g, a, p);
+
+                Console.WriteLine("Generated Parameters:");
+                Console.WriteLine("p (Prime Number): " + p);
+                Console.WriteLine("g (Random Number): " + g);
+                Console.WriteLine("a (Private Key): " + a);
+                Console.WriteLine("A (Public Key): " + A);
+
+                Console.ReadLine();
+            }
+
+            
+        }
+    }
+}
